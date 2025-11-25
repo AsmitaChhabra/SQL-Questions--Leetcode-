42317fe48e504b8dfb78ae36888144b0eda8062e
Asmita Chhabra <asmita.chhabra@flame.edu.in>
2025-11-25 20:30:21 +0530

    Create Delete duplicate emails.sql

---
+++ Delete duplicate emails.sql
@@ -0,0 +1,8 @@
+https://leetcode.com/problems/delete-duplicate-emails/?envType=problem-list-v2&envId=m8baczxh
+  
+
+delete p1 
+from Person p1 
+join Person p2 
+on p1.email = p2.email 
+and p1.id > p2.id 
