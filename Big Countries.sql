83f4d9eb6c4f6cdb0e949a42b35248c6555b2f90
Asmita Chhabra <asmita.chhabra@flame.edu.in>
2025-11-27 09:12:51 +0530

    Create Big Countries.sql

---
+++ Big Countries.sql
@@ -0,0 +1,4 @@
+https://leetcode.com/problems/big-countries/?envType=problem-list-v2&envId=m8baczxh
+
+select name, population , area from World where (population >= 25000000) or (area 
+>=3000000 )
