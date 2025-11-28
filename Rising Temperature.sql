469f055925ad16d1835903464819ea7650c2aab6
Asmita Chhabra <asmita.chhabra@flame.edu.in>
2025-11-28 08:15:00 +0530

    Create  Rising Temperature.sql

---
+++ Rising Temperature.sql
@@ -0,0 +1,7 @@
+https://leetcode.com/problems/rising-temperature/?envType=problem-list-v2&envId=m8baczxh
+
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+    ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature;
