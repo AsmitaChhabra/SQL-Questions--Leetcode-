c104f81a854b6c14ca8b6c6d4b42cbec12c71654
Asmita Chhabra <asmita.chhabra@flame.edu.in>
2026-02-06 12:57:43 +0530

    Create Project Employees I.py

---
+++ Project Employees I.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/project-employees-i/?envType=problem-list-v2&envId=m8baczxh
+
+
+
+select project_id, ROUND(AVG(experience_years),2) as average_years
+from Project p left join Employee e 
+on p.employee_id = e.employee_id 
+group by project_id 
+
